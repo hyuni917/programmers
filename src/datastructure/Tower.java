4e14652eff145787adb3b70fe088cdf152a56e8e
kyung hyun kim <hyuni917@gmail.com>
2020-02-23 19:17:07 +0900

    탑

--- src/datastructure/Tower.java
+++ src/datastructure/Tower.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.Stack;
+
 public class Tower {
 
     public static void main(String[] args){
@@ -9,8 +11,31 @@ public class Tower {
     }
 
     public int[] solution(int[] heights) {
-        int[] answer = {};
+        int heightsSize = heights.length;
+        int[] answer = new int[heightsSize];
+
+        Stack<Integer> stack = new Stack<>();
+        for(int height : heights){
+            stack.push(height);
+        }
+        //System.out.println("heightsSize="+heightsSize);
+        int cnt = 1;
+        do{
+            int sender = stack.pop();
+
+            //System.out.println("sender="+sender);
+            for(int i=heightsSize-cnt; i>=0; i--){
+                if(heights[i]>sender){
+                    //System.out.println("i="+i);
+                    //System.out.println(heights[i]);
+                    answer[heightsSize-cnt] = i+1;
+                    break;
+                }
+            }
+            cnt++;
+            //System.out.println("stack.size()="+stack.size());
+        }while(!stack.empty());
+
         return answer;
     }
-
 }
