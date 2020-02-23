68967d542066c78952c034765bbd7c612e8f7f52
kyung hyun kim <hyuni917@gmail.com>
2020-02-23 18:21:46 +0900

    탑

---
+++ src/datastructure/Tower.java
@@ -0,0 +1,16 @@
+package datastructure;
+
+public class Tower {
+
+    public static void main(String[] args){
+        int numbers[] = {6,9,5,7,4};
+        Tower t= new Tower();
+        t.solution(numbers);
+    }
+
+    public int[] solution(int[] heights) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
